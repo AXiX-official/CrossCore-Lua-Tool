df5d965790ea554005de34b86cd25612d66e9236
AXiX-official <2879710747@qq.com>
2024-02-19 18:32:48 +0800

    add ver.bytes support

--- CrossCore-Lua-Tool/Program.cs
+++ CrossCore-Lua-Tool/Program.cs
@@ -1,68 +1,56 @@
-﻿namespace CrossCore
+﻿using CommandLine;
+
+namespace CrossCore
 {
+    public class Options
+    {
+        [Option('i', "infile", Required = true, HelpText = "Input lua file to be processed.")]
+        public string InFile { get; set; }
+
+        [Option('o', "outfile", Required = true, HelpText = "Output processed lua file.")]
+        public string OutFile { get; set; }
+
+        [Option('e', "encrypt", Default = false, HelpText = "Encrypt the lua file.")]
+        public bool Encrypt { get; set; }
+
+        [Option('d', "decrypt", Default = false, HelpText = "Decrypt the lua file.")]
+        public bool Decrypt { get; set; }
+
+        [Option("nofix", Default = false, HelpText = "Do not fix the file.")]
+        public bool NoFix { get; set; }
+        
+        [Option("ver-bytes", Default = false, HelpText = "Read version data from file.")]
+        public bool VerBytes { get; set; }
+        
+        [Option('h', "help", Default = false, HelpText = "Show help.")]
+        public bool Help { get; set; }
+    }
+    
     internal static class Program
     {
         static void Main(string[] args)
         {
-            string rawfile = "";
-            string outfile = "";
-            bool encrypt = false;
-            bool decrypt = false;
-            bool fix = true;
-
-            for (int i = 0; i < args.Length; i++)
-            {
-                switch (args[i])
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(o =>
                 {
-                    case "--rawfile":
-                    case "-r":
-                        if (i + 1 < args.Length)
-                        {
-                            rawfile = args[++i];
-                        }
-                        break;
-                    case "--outfile":
-                    case "-o":
-                        if (i + 1 < args.Length)
-                        {
-                            outfile = args[++i];
-                        }
-                        break;
-                    case "--encrypt":
-                    case "-en":
-                        encrypt = true;
-                        break;
-                    case "--decrypt":
-                    case "-de":
-                        decrypt = true;
-                        break;
-                    case "--nofix":
-                        fix = false;
-                        break;
-                    case "--help":
-                    case "-h":
-                        Console.WriteLine("Usage: --rawfile <rawfile> --outfile <outfile> [--encrypt] [--decrypt]");
-                        return;
-                    default:
-                        Console.WriteLine($"Unknown argument: {args[i]}");
-                        break;
-                }
-            }
-
-            if (string.IsNullOrEmpty(rawfile) || string.IsNullOrEmpty(outfile))
-            {
-                Console.WriteLine("Usage: --rawfile <rawfile> --outfile <outfile> [--encrypt] [--decrypt]");
-                return;
-            }
-
-            if (encrypt)
-            {
-                LuaScripts.GenEncryptABData(rawfile, outfile, fix);
-            }
-            else if (decrypt)
-            {
-                LuaScripts.GenDecryptABData(rawfile, outfile);
-            }
+                    if (o.Encrypt)
+                    {
+                        LuaScripts.GenEncryptABData(o.InFile, o.OutFile, !o.NoFix);
+                    }
+                    else if (o.Decrypt)
+                    {
+                        LuaScripts.GenDecryptABData(o.InFile, o.OutFile);
+                    }
+                    else if (o.VerBytes)
+                    {
+                        VerMgr verMgr = new VerMgr();
+                        verMgr.LoadVerInfo(o.InFile, o.OutFile);
+                    }
+                    else if (o.Help)
+                    {
+                        Console.WriteLine("Help");
+                    }
+                });
         }
     }
 }
--- README.md
+++ README.md
@@ -4,14 +4,16 @@
 
 ## Command Line Arguments
 
-- `--rawfile <rawfile>` or `-r <rawfile>`: Specifies the raw file to be processed.
+- `--infile <infile>` or `-i <infile>`: Specifies the input lua file to be processed.
 - `--outfile <outfile>` or `-o <outfile>`: Specifies the output file where the processed data will be written.
-- `--encrypt`: Encrypts the raw file.
-- `--decrypt`: Decrypts the raw file.
-
+- `--encrypt` or `-e`: Encrypts the lua file.
+- `--decrypt` or `-d`: Decrypts the lua file.
+- `--nofix`: Do not fix the file.
+- `--ver-bytes`: Read version data from file.
+- `--help` or `-h`: Show help.
 
 Example usage:
 
 ```bash
-./crosscore-lua-tool --rawfile ./path/to/rawfile --outfile ./path/to/outfile --encrypt
+./crosscore-lua-tool --infile ./path/to/infile --outfile ./path/to/outfile --encrypt
 ```
